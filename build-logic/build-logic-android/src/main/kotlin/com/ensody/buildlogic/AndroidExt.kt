772b8a7a7494df4077afeb6f11b3a2b458cf1ad0
Waldemar Kornewald <wkornewald@ensody.com>
2025-12-24 11:36:48 +0100

    Bump NativeBuilds Gradle plugin

--- build-logic/build-logic-android/src/main/kotlin/com/ensody/buildlogic/AndroidExt.kt
+++ build-logic/build-logic-android/src/main/kotlin/com/ensody/buildlogic/AndroidExt.kt
@@ -1,6 +1,7 @@
 package com.ensody.buildlogic
 
 import com.android.build.gradle.TestedExtension
+import com.ensody.nativebuilds.substituteAndroidNativeLibsInUnitTests
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
@@ -58,29 +59,5 @@ fun Project.setupAndroid(
         }
     }
 
-    // In Android unit tests (e.g. the KMP androidUnitTest sourceSet) we can't load Android JNI libs because the tests
-    // run on the host system (Linux, macOS, Windows) instead of an Android emulator.
-    // Normally, KMP Android dependencies only contain the embedded native libraries targeting Android hosts.
-    // In order to run unit tests with native libs for the host we have to substitute all JNI-using dependencies.
-    // The following rule is primarily meant to make all NativeBuilds work automatically:
-    // See the project site for more information: https://github.com/ensody/native-builds
-    configurations.all {
-        // Only apply substitution to Android unit tests
-        if (name.endsWith("UnitTestRuntimeClasspath")) {
-            resolutionStrategy.eachDependency {
-                // The NativeBuilds can all safely be substituted. They contain only the native shared libraries, but
-                // no JNI code.
-                if (requested.group == "com.ensody.nativebuilds" && requested.name.endsWith("-android") ||
-                    // The NativeBuilds libs always get integrated in some JNI wrapper module that contains the actual
-                    // JNI code. By convention we call these wrapper modules "...--native-wrapper" and the final
-                    // KMP Android artifact gets published to Maven with an additional "-android" suffix.
-                    // This rule should result in a relatively low chance of false positives.
-                    requested.name.endsWith("--nativelib-android")
-                ) {
-                    // Replace with the -jvm artifact
-                    useTarget("${requested.group}:${requested.name.removeSuffix("-android")}-jvm:${requested.version}")
-                }
-            }
-        }
-    }
+    substituteAndroidNativeLibsInUnitTests()
 }
