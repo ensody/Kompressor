1349ef5cba89251b92520c3364ae63313a6a3a10
Tapio Ikimaa <tapio@ikimaa.fi>
2026-02-20 08:48:50 +0000

    Add version constraints to kompressor-bom

--- kompressor-bom/build.gradle.kts
+++ kompressor-bom/build.gradle.kts
@@ -5,4 +5,20 @@ plugins {
     id("com.ensody.build-logic.publish")
 }
 
-setupBuildLogic {}
+setupBuildLogic {
+    dependencies {
+        constraints {
+            api(project(":kompressor-brotli--nativelib"))
+            api(project(":kompressor-brotli-ktor"))
+            api(project(":kompressor-core"))
+            api(project(":kompressor-js"))
+            api(project(":kompressor-kotlinx-io"))
+            api(project(":kompressor-ktor"))
+            api(project(":kompressor-test"))
+            api(project(":kompressor-zlib--nativelib"))
+            api(project(":kompressor-zlib-ktor"))
+            api(project(":kompressor-zstd--nativelib"))
+            api(project(":kompressor-zstd-ktor"))
+        }
+    }
+}
