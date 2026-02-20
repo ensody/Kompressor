ee542084ea2434da5207ebd53aecddb24c4ce90d
Tapio Ikimaa <tapio@ikimaa.fi>
2026-02-20 13:59:50 +0200

    Remove hard-coding, tweak kmp build logic to evaluate before collecting version constraints

--- kompressor-bom/build.gradle.kts
+++ kompressor-bom/build.gradle.kts
@@ -5,20 +5,4 @@ plugins {
     id("com.ensody.build-logic.publish")
 }
 
-setupBuildLogic {
-    dependencies {
-        constraints {
-            api(project(":kompressor-brotli--nativelib"))
-            api(project(":kompressor-brotli-ktor"))
-            api(project(":kompressor-core"))
-            api(project(":kompressor-js"))
-            api(project(":kompressor-kotlinx-io"))
-            api(project(":kompressor-ktor"))
-            api(project(":kompressor-test"))
-            api(project(":kompressor-zlib--nativelib"))
-            api(project(":kompressor-zlib-ktor"))
-            api(project(":kompressor-zstd--nativelib"))
-            api(project(":kompressor-zstd-ktor"))
-        }
-    }
-}
+setupBuildLogic {}
